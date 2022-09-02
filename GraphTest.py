6ea1c438865a8cd4de1b267b540671733ebc0b6b
Scott Sigman <ssigman@deuey.wdu>
2022-09-02 15:47:43 -0500

    Documentation completed on graph

--- GraphTest.py
+++ GraphTest.py
@@ -1,15 +1,16 @@
 
 import numpy as np
 import graph
-from graph import ADJ_MAT, EDGE
+from graph import Adj_Mat, Edge
 
 if __name__ == "__main__":
     verts: list[str] = ['a', 'b', 'c']
-    edges: list[EDGE] = [('a','b'), ('c','a')]
-    g = graph.GraphAM(verts, directed=True)
+    edges: list[Edge] = [('a','b'), ('c','a')]
+    g = graph.GraphAM(verts)
     g.addEdges(edges)
     print('The verticies are ')
     print(*g.getV(), sep=',')
     print('The empty graph')
     print(g.getGraph())
+
 
--- graph.py
+++ graph.py
@@ -7,22 +7,42 @@
 #                matrix - the matrix is an numpy array
 #    * GraphAL - graph stored using an adjacency list -
 #                TO BE Implemented (9/1/2022)
+#    Methods
+#     Unless specified, methods belong to both graph storage classes.
+#      * consructor
+#          Graph{AM|AL}(self, verts :list[str], edges :list[Edges]=[],
+#                          directed :bool = False, eleType :str ='i' ) -> None
+#             eleType in {'i','i1','i2','i4','i8','f2','f4','f8'} ie. numpy
+#             dtype character code abbreviations
+#      * getV(self) -> list[str]
+#      * GraphAM.getGraph(self) -> Adj_Mat
+#      * GraphAL.getGraph(self) -> Type to be determined.
+#      * addEdges(self, edges :list[Edge]) -> None
+#      * degree(self, edge :Edge) -> int
+#      * inDegree(self, edge :Edge) -> int, not defined for undirected graph
+#           returns None 
+#      * outDegree(self, edge :Edge) -> int, not defined for undirected graph
+#           returns None
+#
+# Data types
+#   Edge - tuple representing a edge (str, str)
+#   Adj_Mat - adjacency matrix - 2D matrix of equal size   
 #
 # Modification Log:
-#
+#    *  9/2/2022 - Completed documentation. S. Sigman 
 
 import numpy as np
 from nptyping import NDArray, Shape, Integer
 from typing import Any
 
-EDGE = tuple[str,str]
-ADJ_MAT = NDArray[Shape['Any, ...'], Integer]
+Edge = tuple[str,str]
+Adj_Mat = NDArray[Shape['Dim, Dim'], Integer]
 
 class GraphAM:
-    def __init__(self, verts: list[str], edges: list[EDGE]=[], *, directed :bool=False, eleType='i') -> None:
+    def __init__(self, verts: list[str], edges: list[Edge]=[], *, directed :bool =False, eleType :str ='i') -> None:
         self.V :list[str] = verts
         self.directed :bool = directed
-        self.graph :ADJ_MAT = np.zeros((len(verts),len(verts)), dtype=eleType)
+        self.graph :Adj_Mat = np.zeros((len(verts),len(verts)), dtype=eleType)
         # add edges if any are specified
         if edges != []:
             for e in edges:
@@ -41,11 +61,11 @@ class GraphAM:
         return(self.V)
 
     # Method to return the graph object
-    def getGraph(self) -> ADJ_MAT:
+    def getGraph(self) -> Adj_Mat:
         return(self.graph)
 
     # Method to add the list of edges to the graph
-    def addEdges(self, edges :list[EDGE]):
+    def addEdges(self, edges :list[Edge]) -> None: 
         if edges != []:
             for e in edges:
                 # get index of start and end vertices
@@ -56,3 +76,20 @@ class GraphAM:
                 # if undirected add symmetric edge
                 if not self.directed:
                     self.graph[end,start] = 1
+
+    # Method to return the degree of an edge.  In a directed
+    # graph it returns degree = indegree + outdegree
+    def degree(self, edge : Edge) -> int:
+        pass  # stub - add the body
+
+    # Method to return the in-degree of a node in a directed
+    # graph.  In the case of an undirected graph, None is 
+    # returned.
+    def inDegree(self, edge :Edge) -> int:
+        pass # stub - add the body
+
+    # Method to return the out-degree of a node in a 
+    # directed graph.  In the case of an undirected
+    # graph, None is returned.
+    def outDegree(self, edge :Edge) -> int:
+        pass # stub - add the body
